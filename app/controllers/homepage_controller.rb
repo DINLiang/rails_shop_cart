21ce6db7b639a333d8d2d74447bb5c730a2bf2ce
DINLiang <15383332930@163.com>
2015-04-24 18:12:05 +0800

    实现购物车数量的变化

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,8 +1,28 @@
 class HomepageController < ApplicationController
   def index
+    @number = CartList.all
+    @cart_num = 0
+    @number.each do |i|
+      @cart_num += i.count
+    end
+    @cart_num = @cart_num
   end
+  # def get_cart_num
+  #   @number = CartList.all
+  #   @@cart_num = 0
+  #   @number.each do |i|
+  #     @@cart_num += i.count
+  #   end
+  # end
   def shop_list
      @list = ShopList.all
+     @cart_num = @cart_num
+
+     @number = CartList.all
+     @cart_num = 0
+     @number.each do |i|
+       @cart_num += i.count
+     end
   end
   def add_cart
     shop = ShopList.find_by_id(params[:id])
@@ -16,7 +36,7 @@ class HomepageController < ApplicationController
       list = CartList.new
       list.name = shop.name
       list.sort = shop.sort
-      #list.barcode = shop.barcode
+      list.barcode = shop.barcode
       list.unit = shop.unit
       list.price = shop.price
       list.count = 1
@@ -29,6 +49,13 @@ class HomepageController < ApplicationController
 
   def shopping_cart
     @goods = CartList.all
+    @cart_num = @cart_num
+
+    @number = CartList.all
+    @cart_num = 0
+    @number.each do |i|
+      @cart_num += i.count
+    end
     @sum_total = 0
     @goods.each do |k|
       @sum_total += k.sum
@@ -48,6 +75,7 @@ class HomepageController < ApplicationController
           else
             list = FreeList.new
             list.name = i.name
+            list.barcode = i.barcode
             list.sort = i.sort
             list.count = (i.count / 3).to_i
             list.save
@@ -61,12 +89,16 @@ class HomepageController < ApplicationController
   def pay_list
     @goods = CartList.all
     @free_list = FreeList.all
+    @cart_num = @cart_num
+
+    @cart_num = 0
+    @goods.each do |i|
+      @cart_num += i.count
+    end
   end
   def clear_goods
      CartList.all.delete_all
      FreeList.all.delete_all
-    redirect_to "homepage/shop_list"
   end
-
 
 end
